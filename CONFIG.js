030441c6239e2ce10bb839a0239069b056c7f210
Kastysiks <staskanareika@gmail.com>
2025-02-09 15:04:40 +0300

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+    titleHeader: "Привет, у меня есть кое-что, что я хочу сказать. </br> Ты лахушка❤️❤️",
+  descriptionHeader:
+    "Ты знала об этом?)",
+  buttonYes: "Я согласна",
+  buttonNo: "Не согласна",
+  titleModar: "ееее.",
+  descriptionModar:
+    "❤️❤️Лахушкаа, роднулька моя, люблю тебя❤️❤️. </br> Пасаси бейба❤️❤️.",
+};
